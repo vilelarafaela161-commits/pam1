1c502d5cdce6f36e4af752b83c49d61faa02170b
vilelarafaela161-commits <vilelarafaela161@gmail.com>
2026-09-21 12:22:20 +0000

    v23

--- 3bimestre/nome_do_app/App.js
+++ 3bimestre/nome_do_app/App.js
@@ -1,52 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Slow jamz </Text>
-      
-      Are you gonna be? Say that you're gonna be 
-Oooo, oooo, oooo, oooo, oooooo
-She said she want some Marvin Gaye, some Luther Vandross
-A little Anita will definitely set this party off right
-(are you gonna be, are you gonna be, are you gonna be
-are you gonna be, are you gonna be, well well well well)
-She said she want some Ready for the World, some New Edition
-Some Minnie Ripperton will definitely set this party off right
-(are you gonna be, are you gonna be, are you gonna be
-are you gonna be, are you gonna be, well well well well)
-It definitely will
-You know what I told her
-I told her to drive over in yo new
-whip, bring some friends you cool wit
-I'mma bring the cool whip, then I want you to strip
-See you is my new chick, so we get our grind on
-She be grabbin me, callin me +Biggie+ like +Shyne+ home
-Man I swear she fine homes, why she always lyin though?
-Tellin me them diamonds, when she know they rhinestones
-She got a light-skinned friend look like Michael Jackson
-Got a dark-skinned friend look like Michael Jackson
-I played 'ready for the world', she was ready for some action
-My dawg said you ain't no freak, so you bout to prove my man wrong
-I'mma lay this Vandross, you gon take yo pants off
-I'mma play this Gladys Knight, me and you gon get right
-She said she want some Marvin Gaye, some Luther Vandross
-A little Anita will definitely set this party off right
-(are you gonna be, are you gonna be, are you gonna be
-are you gonna be, are you gonna be, well well well well)
-She said she want some Ready for the World, some New Edition
-Some Minnie Ripperton will definitely set this party off right
-(are you gonna be, are you gonna be, are you gonna be
-are you gonna be, are you gonna be, well well well well)
-Now Kanye, I know I told you to slow it down
-It's good, it's all good, it's beautiful
-But now I need you to do it faster
-baby, can you please - do it faster, do it
-faster
-Damn baby,
-
       <StatusBar style="auto" />
+
+      <ScrollView contentContainerStyle={styles.content}>
+        <Text style={styles.title}>Slow Jamz</Text>
+
+        <Text style={styles.lyrics}>
+          Are you gonna be? Say that you're gonna be{'\n'}
+          Oooo, oooo, oooo, oooo...
+          {'\n\n'}
+          She said she want some Marvin Gaye...
+          {'\n\n'}
+          It definitely will{'\n'}
+          You know what I told her
+        </Text>
+      </ScrollView>
     </View>
   );
 }
@@ -54,12 +26,26 @@ Damn baby,
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-     borderWidth: 15,
+    borderWidth: 15,
     borderColor: 'brown',
-    fontSize: '87',
-
     backgroundColor: '#f8a72c',
-    alignItems: '',
-    justifyContent: 'center',
+  },
+
+  content: {
+    padding: 20,
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+
+  lyrics: {
+    fontSize: 18,
+    lineHeight: 28,
+    textAlign: 'center',
   },
 });
+
